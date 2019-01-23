7a0da3edddfd3931e73cbb6b8c6f533dc1ad5121
Vincent_YD <1217047025@qq.com>
2019-01-23 10:27:16 +0800

    Spring Boot控制事务
    Spring Boot控制事务

---
+++ src/main/java/com/springbootweb/service/UserService.java
@@ -0,0 +1,25 @@
+package com.springbootweb.service;
+
+import com.springbootweb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.springbootweb.domain.User;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public User save(User user) {
+        //保存实体类
+        userRepository.save(user);
+        //修改密码
+        user.setPassword("123456");
+        //重新保存，更新记录
+        return userRepository.save(user);
+    }
+}
+
